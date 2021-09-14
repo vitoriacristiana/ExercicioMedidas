daa35c3e45961f8581b6edb95ad3905c9b977648
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-09-14 12:41:27 -0300

    Resolução Exercício

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ExercicioMedidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+             double m,convcm,convkm;
+
+            Console.Write("Entre com o valor em Metros:");
+            m =double.Parse(Console.ReadLine());
+            convcm = m  * 100;
+            convkm = m  / 1000;        
+            Console.BackgroundColor=ConsoleColor.DarkRed;
+            Console.ForegroundColor=ConsoleColor.Black;
+            Console.WriteLine("\n--- Equivalência ---");
+            Console.ResetColor();
+            Console.WriteLine($"{convcm} cm \n{m} m \n{convkm} km");
+            
+    
+                 }    
+            
+            } 
+    
+          }
+
+
+
+
+
+
+             
+             
+                  
+             
+            
+            
+            
+            
+            
+
+        
+    
+
